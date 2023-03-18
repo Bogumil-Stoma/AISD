dc188f2a596e1363b2fd95c828795ae7d7c9feef
kjurczyn <01169180@pw.edu.pl>
2023-03-18 19:08:02 +0100

    fixed quciksort tests

--- test_sort.py
+++ test_sort.py
@@ -23,9 +23,12 @@ def test_selection_sort():
     assert sort.selection_sort(list('bruh')) == ['b', 'h', 'r', 'u']
 
 def test_quick_sort():
-    assert sort.quick_sort([1,2,3]) == [1,2,3]
-    assert sort.quick_sort([3,2,3]) == [2,3,3]
-    assert sort.quick_sort([3,2,1]) == [1,2,3]
-    assert sort.quick_sort([2,1,3]) == [1,2,3]
-    assert sort.quick_sort(list('bruh')) == ['b', 'h', 'r', 'u']
+    a2 = [1, 7, 4, 1, 10, 9, -2]
+    a3 = ['b', 'r', 'u', 'h']
+    assert sort.quick_sort([1,2,3], 0, 2) == [1,2,3]
+    assert sort.quick_sort([3,2,3], 0, 2) == [2,3,3]
+    assert sort.quick_sort([3,2,1], 0, 2) == [1,2,3]
+    assert sort.quick_sort([2,1,3], 0, 2) == [1,2,3]
+    assert sort.quick_sort(a3, 0, len(a3)-1) == ['b', 'h', 'r', 'u']
+    assert sort.quick_sort(a2, 0, len(a2)-1) == [-2, 1, 1, 4, 7, 9, 10]
     
