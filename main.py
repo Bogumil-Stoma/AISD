0adca3d8752ae85ae7a796dbdd7f92be9e357d4b
kjurczyn <01169180@pw.edu.pl>
2023-03-18 22:45:42 +0100

    added more tests

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ import time
 import gc
 
 text = []
-bubble_t = []
-merge_t = []
-quick_t = []
-selection_t = []
-NUM_OF_TESTS = 5
+# bubble_t = []
+# merge_t = []
+# quick_t = []
+# selection_t = []
+# NUM_OF_TESTS = 5
 
 with open('pan-tadeusz.txt', 'r', encoding='utf-8') as fp:
     for line in fp:
         text.extend(map(str.lower, line.split()))
-
 
 def measure_time(func, n, text):
     gc_old = gc.isenabled()
@@ -25,59 +24,56 @@ def measure_time(func, n, text):
         gc.enable()
     return stop-start
 
+def test_sorting(func, text, min, max, increments):
+    test_t = []
+    test_n = []
+    for i in range(min, max+1, increments):
+        test_t.append(measure_time(func, i, text))
+        test_n.append(i)
+    return test_n, test_t
+
 def main():
-    for i in range(1, NUM_OF_TESTS):
-        bubble_t.append(measure_time(sort.bubble_sort, i*2000, text))
-        merge_t.append(measure_time(sort.merge_sort, i*2000, text))
-        # quick_t.append(measure_time(sort.quick_sort, i*20000, text))
-        selection_t.append(measure_time(sort.selection_sort, i*2000, text))
-    xs = [n*2000 for n in range(1, NUM_OF_TESTS)]
-    plt.plot(xs, bubble_t, label='bubble sort')
-    plt.plot(xs, merge_t, label='merge sort')
-    plt.plot(xs, selection_t, label='selecion sort')
+    # for i in range(1, NUM_OF_TESTS):
+    #     bubble_t.append(measure_time(sort.bubble_sort, i*2000, text))
+    #     merge_t.append(measure_time(sort.merge_sort, i*2000, text))
+    #     quick_t.append(measure_time(sort.quick_sort, i*20000, text))
+    #     selection_t.append(measure_time(sort.selection_sort, i*2000, text))
+    # xs = [n*2000 for n in range(1, NUM_OF_TESTS)]
+    # plt.plot(xs, bubble_t, label='bubble sort')
+    # plt.plot(xs, merge_t, label='merge sort')
+    # plt.plot(xs, selection_t, label='selecion sort')
     # plt.plot(xs, quick_t, label='quick sort')
+    # plt.ylabel('time[s]')
+    # plt.xlabel('num of words')
+    # plt.legend()
+    # plt.savefig('sorting_stats1.png')
+
+    func_list = [sort.bubble_sort, sort.quick_sort, sort.merge_sort, sort.selection_sort]
+    func_out_list = []
+    for x in func_list:
+        func_out_list.append((x, test_sorting(x, text, 1000, 100000, 1000)))
+    
+    for i in range(len(func_out_list)):
+        fname = func_out_list[i][0].__name__
+        data = func_out_list[i][1]
+        plt.figure(10)
+        plt.plot(*data, label = fname)
+        plt.figure(i)
+        plt.ylabel('time[s]')
+        plt.xlabel('num of words')
+        plt.plot(*data)
+        plt.suptitle(fname)
+        plt.savefig(fname+'.png')
+        
+    plt.figure(10)
     plt.ylabel('time[s]')
     plt.xlabel('num of words')
+    plt.suptitle('Sorting Algorithms')
     plt.legend()
     plt.savefig('sorting_stats.png')
-
+    plt.show()
 
 
 if __name__ == '__main__':
     main()
 
-
-# def test_sorting(func, text, min, max, increments):
-#     test_t = []
-#     test_n = []
-#     for i in range(min, max+1, increments):
-#         test_t.append(measure_time(func, i, text))
-#         test_n.append(i)
-#     return test_n, test_t
-
-
-# plt.figure(1)
-# plt.plot(*test_sorting(sort.bubble_sort, text, 1000, 10000, 1000))
-# plt.xlabel('Ilość słów')
-# plt.ylabel('Czas')
-# plt.suptitle('Bubble Sort')
-# plt.savefig('bubble_sort.png')
-# plt.figure(2)
-# plt.plot(*test_sorting(sort.quick_sort, text, 1000, 10000, 1000))
-# plt.xlabel('Ilość słów')
-# plt.ylabel('Czas')
-# plt.suptitle('Quick Sort')
-# plt.savefig('quick_sort.png')
-# plt.figure(3)
-# plt.plot(*test_sorting(sort.merge_sort, text, 1000, 10000, 1000))
-# plt.xlabel('Ilość słów')
-# plt.ylabel('Czas')
-# plt.suptitle('Merge Sort')
-# plt.savefig('merge_sort.png')
-# plt.figure(4)
-# plt.plot(*test_sorting(sort.selection_sort, text, 1000, 10000, 1000))
-# plt.xlabel('Ilość słów')
-# plt.ylabel('Czas')
-# plt.suptitle('Selection Sort')
-# plt.savefig('selection_sort.png')
-# plt.show()
--- test_sort.py
+++ test_sort.py
@@ -1,26 +1,41 @@
 import sort
 
+text = []
+with open('pan-tadeusz.txt', 'r', encoding='utf-8') as fp:
+    for line in fp:
+        text.extend(map(str.lower, line.split()))
+        if len(text) >= 100:
+            break
+text_sorted = sorted(text)
+
+
 def test_bubble_sort():
+    text_sorted = sorted(text)
     assert sort.bubble_sort([1,2,3]) == [1,2,3]
     assert sort.bubble_sort([3,2,3]) == [2,3,3]
     assert sort.bubble_sort([3,2,1]) == [1,2,3]
     assert sort.bubble_sort([2,1,3]) == [1,2,3]
     assert sort.bubble_sort(list('bruh')) == ['b', 'h', 'r', 'u']
+    assert sort.bubble_sort(text) == text_sorted
 
 
 def test_merge_sort():
+    text_sorted = sorted(text)
     assert sort.merge_sort([1,2,3]) == [1,2,3]
     assert sort.merge_sort([3,2,3]) == [2,3,3]
     assert sort.merge_sort([3,2,1]) == [1,2,3]
     assert sort.merge_sort([2,1,3]) == [1,2,3]
     assert sort.merge_sort(list('bruh')) == ['b', 'h', 'r', 'u']
+    assert sort.merge_sort(text) == text_sorted
 
 def test_selection_sort():
+    text_sorted = sorted(text)
     assert sort.selection_sort([1,2,3]) == [1,2,3]
     assert sort.selection_sort([3,2,3]) == [2,3,3]
     assert sort.selection_sort([3,2,1]) == [1,2,3]
     assert sort.selection_sort([2,1,3]) == [1,2,3]
     assert sort.selection_sort(list('bruh')) == ['b', 'h', 'r', 'u']
+    assert sort.selection_sort(text) == text_sorted
 
 def test_quick_sort():
     a2 = [1, 7, 4, 1, 10, 9, -2]
@@ -31,4 +46,5 @@ def test_quick_sort():
     assert sort.quick_sort([2,1,3], 0, 2) == [1,2,3]
     assert sort.quick_sort(a3, 0, len(a3)-1) == ['b', 'h', 'r', 'u']
     assert sort.quick_sort(a2, 0, len(a2)-1) == [-2, 1, 1, 4, 7, 9, 10]
+    assert sort.quick_sort(text) == text_sorted
     
