72d325b59681222d79d71754d1c446496eb08a44
bstoma <bogumil.stoma@gmail.com>
2023-03-22 23:08:21 +0100

    final changes

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import matplotlib.pyplot as plt
-import sort
+import sort_lib
 import time
 import gc
 
 text = []
-# bubble_t = []
-# merge_t = []
-# quick_t = []
-# selection_t = []
-# NUM_OF_TESTS = 5
-
 with open('pan-tadeusz.txt', 'r', encoding='utf-8') as fp:
     for line in fp:
         text.extend(map(str.lower, line.split()))
@@ -33,26 +27,11 @@ def test_sorting(func, text, min, max, increments):
     return test_n, test_t
 
 def main():
-    # for i in range(1, NUM_OF_TESTS):
-    #     bubble_t.append(measure_time(sort.bubble_sort, i*2000, text))
-    #     merge_t.append(measure_time(sort.merge_sort, i*2000, text))
-    #     quick_t.append(measure_time(sort.quick_sort, i*20000, text))
-    #     selection_t.append(measure_time(sort.selection_sort, i*2000, text))
-    # xs = [n*2000 for n in range(1, NUM_OF_TESTS)]
-    # plt.plot(xs, bubble_t, label='bubble sort')
-    # plt.plot(xs, merge_t, label='merge sort')
-    # plt.plot(xs, selection_t, label='selecion sort')
-    # plt.plot(xs, quick_t, label='quick sort')
-    # plt.ylabel('time[s]')
-    # plt.xlabel('num of words')
-    # plt.legend()
-    # plt.savefig('sorting_stats1.png')
-
-    func_list = [sort.bubble_sort, sort.quick_sort, sort.merge_sort, sort.selection_sort]
+    func_list = [sort_lib.bubble_sort, sort_lib.quick_sort, sort_lib.merge_sort, sort_lib.selection_sort]
     func_out_list = []
     for x in func_list:
         func_out_list.append((x, test_sorting(x, text, 1000, 100000, 1000)))
-    
+
     for i in range(len(func_out_list)):
         fname = func_out_list[i][0].__name__
         data = func_out_list[i][1]
@@ -64,7 +43,7 @@ def main():
         plt.plot(*data)
         plt.suptitle(fname)
         plt.savefig(fname+'.png')
-        
+
     plt.figure(10)
     plt.ylabel('time[s]')
     plt.xlabel('num of words')
--- test_sort.py
+++ test_sort.py
@@ -1,50 +1,33 @@
-import sort
-
-text = []
-with open('pan-tadeusz.txt', 'r', encoding='utf-8') as fp:
-    for line in fp:
-        text.extend(map(str.lower, line.split()))
-        if len(text) >= 100:
-            break
-text_sorted = sorted(text)
-
+import sort_lib
 
 def test_bubble_sort():
-    text_sorted = sorted(text)
-    assert sort.bubble_sort([1,2,3]) == [1,2,3]
-    assert sort.bubble_sort([3,2,3]) == [2,3,3]
-    assert sort.bubble_sort([3,2,1]) == [1,2,3]
-    assert sort.bubble_sort([2,1,3]) == [1,2,3]
-    assert sort.bubble_sort(list('bruh')) == ['b', 'h', 'r', 'u']
-    assert sort.bubble_sort(text) == text_sorted
+    assert sort_lib.bubble_sort([1,2,3]) == [1,2,3]
+    assert sort_lib.bubble_sort([3,2,3]) == [2,3,3]
+    assert sort_lib.bubble_sort([3,2,1]) == [1,2,3]
+    assert sort_lib.bubble_sort([2,1,3]) == [1,2,3]
+    assert sort_lib.bubble_sort(list('bruh')) == ['b', 'h', 'r', 'u']
 
 
 def test_merge_sort():
-    text_sorted = sorted(text)
-    assert sort.merge_sort([1,2,3]) == [1,2,3]
-    assert sort.merge_sort([3,2,3]) == [2,3,3]
-    assert sort.merge_sort([3,2,1]) == [1,2,3]
-    assert sort.merge_sort([2,1,3]) == [1,2,3]
-    assert sort.merge_sort(list('bruh')) == ['b', 'h', 'r', 'u']
-    assert sort.merge_sort(text) == text_sorted
+    assert sort_lib.merge_sort([1,2,3]) == [1,2,3]
+    assert sort_lib.merge_sort([3,2,3]) == [2,3,3]
+    assert sort_lib.merge_sort([3,2,1]) == [1,2,3]
+    assert sort_lib.merge_sort([2,1,3]) == [1,2,3]
+    assert sort_lib.merge_sort(list('bruh')) == ['b', 'h', 'r', 'u']
 
 def test_selection_sort():
-    text_sorted = sorted(text)
-    assert sort.selection_sort([1,2,3]) == [1,2,3]
-    assert sort.selection_sort([3,2,3]) == [2,3,3]
-    assert sort.selection_sort([3,2,1]) == [1,2,3]
-    assert sort.selection_sort([2,1,3]) == [1,2,3]
-    assert sort.selection_sort(list('bruh')) == ['b', 'h', 'r', 'u']
-    assert sort.selection_sort(text) == text_sorted
+    assert sort_lib.selection_sort([1,2,3]) == [1,2,3]
+    assert sort_lib.selection_sort([3,2,3]) == [2,3,3]
+    assert sort_lib.selection_sort([3,2,1]) == [1,2,3]
+    assert sort_lib.selection_sort([2,1,3]) == [1,2,3]
+    assert sort_lib.selection_sort(list('bruh')) == ['b', 'h', 'r', 'u']
 
 def test_quick_sort():
     a2 = [1, 7, 4, 1, 10, 9, -2]
     a3 = ['b', 'r', 'u', 'h']
-    assert sort.quick_sort([1,2,3], 0, 2) == [1,2,3]
-    assert sort.quick_sort([3,2,3], 0, 2) == [2,3,3]
-    assert sort.quick_sort([3,2,1], 0, 2) == [1,2,3]
-    assert sort.quick_sort([2,1,3], 0, 2) == [1,2,3]
-    assert sort.quick_sort(a3, 0, len(a3)-1) == ['b', 'h', 'r', 'u']
-    assert sort.quick_sort(a2, 0, len(a2)-1) == [-2, 1, 1, 4, 7, 9, 10]
-    assert sort.quick_sort(text) == text_sorted
-    
+    assert sort_lib.quick_sort([1,2,3], 0, 2) == [1,2,3]
+    assert sort_lib.quick_sort([3,2,3], 0, 2) == [2,3,3]
+    assert sort_lib.quick_sort([3,2,1], 0, 2) == [1,2,3]
+    assert sort_lib.quick_sort([2,1,3], 0, 2) == [1,2,3]
+    assert sort_lib.quick_sort(a3, 0, len(a3)-1) == ['b', 'h', 'r', 'u']
+    assert sort_lib.quick_sort(a2, 0, len(a2)-1) == [-2, 1, 1, 4, 7, 9, 10]
