38601db517413b5b755b3668ac836e4f7921b1e9
kjurczyn <01169180@pw.edu.pl>
2023-03-23 22:26:54 +0100

    small changes, regenerated images

--- main.py
+++ main.py
@@ -25,26 +25,32 @@ def measure_time(func, n, text):
 def test_sorting(func, text, min, max, increments):
     test_t = []
     test_n = []
+    #  for i in range(min, max+1, increments):
+    #     if ((func.__name__ == "bubble_sort" or func.__name__ == "selection_sort") and i > 4000):
+    #         #limit for bubble and selection sort so it doesnt take so much time
+    #         continue
     for i in range(min, max+1, increments):
-        if ((func.__name__ == "bubble_sort" or func.__name__ == "selection_sort") and i > 4000):
-            #limit for bubble and selection sort so it doesnt take so much time
-            continue
-
         del_time = 0
 
-        for j in range(NUM_OF_MEASUREMENTS):
+        for _ in range(NUM_OF_MEASUREMENTS):
             del_time += measure_time(func, i, text)
         del_time = del_time/NUM_OF_MEASUREMENTS
         test_t.append(del_time)
         test_n.append(i)
-        print(f'num of iteration {i}')
+        print(f'num of iteration (words) {i}')
     return test_n, test_t
 
 def main():
-    func_list = [sort_lib.bubble_sort, sort_lib.quick_sort, sort_lib.merge_sort, sort_lib.selection_sort]
+    # func_list = [sort_lib.bubble_sort, sort_lib.quick_sort, sort_lib.merge_sort, sort_lib.selection_sort]
+    #boilerplate bo nie ma sensu pisać pętli, każde argumenty właściwie inne
     func_out_list = []
-    for x in func_list:
-        func_out_list.append((x, test_sorting(x, text, 1000, 40000, 1000)))
+    func_out_list.append((sort_lib.bubble_sort, test_sorting(sort_lib.bubble_sort, text, 1000, 4000, 1000)))
+    func_out_list.append((sort_lib.selection_sort, test_sorting(sort_lib.selection_sort, text, 1000, 6000, 1000)))
+    func_out_list.append((sort_lib.quick_sort, test_sorting(sort_lib.quick_sort, text, 1000, 20000, 1000)))
+    func_out_list.append((sort_lib.merge_sort, test_sorting(sort_lib.merge_sort, text, 1000, 20000, 1000)))
+
+    # for x in func_list:         
+    #     func_out_list.append((x, test_sorting(x, text, 1000, 20000, 1000)))
 
     for i in range(len(func_out_list)):
         fname = func_out_list[i][0].__name__
