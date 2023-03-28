adb41c260cf500d8b80992c78e0e1fd819908899
kjurczyn <01169180@pw.edu.pl>
2023-03-28 23:01:29 +0200

    removed unnecessary code

--- sort_lib.py
+++ sort_lib.py
@@ -69,60 +69,6 @@ def quick_sort_random(a, l=0, r=None) -> list:
         quick_sort(a=a, l=i, r=r)
     return a
 
-def quicksort(arr, start , stop):
-    if(start < stop):
-         
-        # pivotindex is the index where
-        # the pivot lies in the array
-        pivotindex = partitionrand(arr,\
-                             start, stop)
-         
-        # At this stage the array is
-        # partially sorted around the pivot.
-        # Separately sorting the
-        # left half of the array and the
-        # right half of the array.
-        quicksort(arr , start , pivotindex-1)
-        quicksort(arr, pivotindex + 1, stop)
-    return arr
- 
-# This function generates random pivot,
-# swaps the first element with the pivot
-# and calls the partition function.
-def partitionrand(arr , start, stop):
- 
-    # Generating a random number between the
-    # starting index of the array and the
-    # ending index of the array.
-    randpivot = randrange(start, stop)
- 
-    # Swapping the starting element of
-    # the array and the pivot
-    arr[start], arr[randpivot] = \
-        arr[randpivot], arr[start]
-    return partition(arr, start, stop)
-
-def partition(arr,start,stop):
-    pivot = start # pivot
-     
-    # a variable to memorize where the
-    i = start + 1
-     
-    # partition in the array starts from.
-    for j in range(start + 1, stop + 1):
-         
-        # if the current element is smaller
-        # or equal to pivot, shift it to the
-        # left side of the partition.
-        if arr[j] <= arr[pivot]:
-            arr[i] , arr[j] = arr[j] , arr[i]
-            i = i + 1
-    arr[pivot] , arr[i - 1] =\
-            arr[i - 1] , arr[pivot]
-    pivot = i - 1
-    return (pivot)
- 
-
 def selection_sort(a: list) -> list:
     for x in range(len(a)):
         min_index = x
