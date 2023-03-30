0a66c2a7550bda56dc706e52f9984b807a688cbf
bstoma <bogumil.stoma@gmail.com>
2023-03-30 15:30:12 +0200

    implemented insert

--- tree.py
+++ tree.py
@@ -5,24 +5,36 @@ class bstNode:
         self.val = val
         self.l_child = None
         self.r_child = None
-    
+
     def setRightChild(self, val):
         self.r_child = bstNode(val)
 
     def setLeftChild(self, val):
         self.l_child = bstNode(val)
-    
+
     def getRightChild(self):
         return self.r_child
-    
+
     def getLeftChild(self):
         return self.l_child
 
 class BST:
     def __init__(self, root):
-        self.root = root
+        self.root:bstNode = root
     def insert(self, val):
-       pass
+        node = self.root
+        last_node = node
+        while node != None:
+            if val >= node.val:
+                last_node = node
+                node = node.getRightChild()
+            else:
+                last_node = node
+                node = node.getLeftChild()
+        if val >= last_node.val:
+            last_node.setRightChild(val)
+        else:
+            last_node.setLeftChild(val)
 
     def find(self, val):
         pass
