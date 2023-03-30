f8094faac085ac2dd1d1b01c8f630ee085cbc743
kjurczyn <01169180@pw.edu.pl>
2023-03-30 15:06:39 +0200

    added main

---
+++ main.py
@@ -0,0 +1,8 @@
+import tree
+
+def main():
+    pass
+
+
+if __name__=="__main__":
+    main()
--- tree.py
+++ tree.py
@@ -1,13 +1,16 @@
 
-class BST:
+class bstNode:
     def __init__(self, val) -> None:
         self.val = val
         self.l_child = None
         self.r_child = None
-    
+
+class BST:
+    def __init__(self, root):
+        self.node_list = []
     def insert(self, val):
         pass
-
+    
     def find(self, val):
         pass
 
