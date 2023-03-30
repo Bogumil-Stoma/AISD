c042e1f30195b7dfe6b4e486355015d25a97a24d
kjurczyn <01169180@pw.edu.pl>
2023-03-30 14:49:16 +0200

    Added class BST

---
+++ tree.py
@@ -0,0 +1,18 @@
+
+class BST:
+    def __init__(self, val) -> None:
+        self.val = val
+        self.l_child = None
+        self.r_child = None
+    
+    def insert(self, val):
+        pass
+
+    def find(self, val):
+        pass
+
+    def remove(self, val):
+        pass
+
+    def print(self):
+        pass
