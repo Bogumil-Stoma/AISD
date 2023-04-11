0b6f26342dba6beda9ae824e7c3c1fa510696c4d
kjurczyn <01169180@pw.edu.pl>
2023-04-11 23:12:51 +0200

    added some tests

--- test_avl.py
+++ test_avl.py
@@ -1,1 +1,17 @@
 from tree import avlTree, avlNode
+
+def test_traverse_inorder():
+    node1 = avlNode(10)
+    node2 = avlNode(5)
+    node3 = avlNode(15)
+    node4 = avlNode(20)
+    node5 = avlNode(11)
+
+def test_insert_1():
+    pass
+
+def test_insert_2():
+    pass
+
+def test_find():
+    pass
