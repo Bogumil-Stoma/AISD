9d479cc0c5ad017d96b2c328a3a7e9ac5760c436
kjurczyn <01169180@pw.edu.pl>
2023-04-27 15:23:53 +0200

    completed tests

--- pattern_sarch.py
+++ pattern_sarch.py
@@ -3,7 +3,9 @@
 def searchN(word: str, text: str) -> int:
     w_len = len(word)
     t_len = len(text)
-
+    if w_len == 0 or t_len == 0 or w_len > t_len:
+        return -1
+    
     for i in range(t_len - w_len + 1):
         if text[i:i+w_len] == word:
             return i
--- test_pattern_search.py
+++ test_pattern_search.py
@@ -1,19 +1,66 @@
 from pattern_sarch import searchN, searchKMP, searchKR
 
+def simple(func):
+    """Prosty test"""
+    text = "AABBCCDDEEFFGGHHIIJJ"
+    assert(func("HH", text)==14)
+    assert(func("LL", text)==-1)
+
+def empty(func):
+        """Test z pustymi stringami"""
+        text = "AABBCCDDEEFFGGHHIIJJ"
+        assert(func("", text)==-1)
+        assert(func("", "")==-1)
+
+def equal(func):
+        """Test stringów równej długości"""
+        text = "AABBCCDDEEFFGGHHIIJJ"
+        assert(func(text, text)==0)
+        assert(func("AAGGCCDDEEFFGGHHLLJJ", text)==-1)
+
+def longer(func):
+        """Test z dłuższym słowem niż tekstem"""
+        text = "AABBCCDDEEFFGGHHIIJJ"
+        word = "AABBCCDDEEFFGGHHIIJJKK"
+        assert(func(word, text)==-1)
+
 class TestNaive:
     def test_simple(self):
-        text = "AABBCCDDEEFFGGHHIIJJ"
-        word = "HH"
-        assert(searchN(word, text)==14)
+        simple(searchN)
+    
+    def test_empty(self):
+        empty(searchN)
+
+    def test_equal(self):
+        equal(searchN)
+
+    def test_longer(self):
+        longer(searchN)
+
 
 class TestKMP:
     def test_simple(self):
-        text = "AABBCCDDEEFFGGHHIIJJ"
-        word = "HH"
-        assert(searchKMP(word, text)==14)
+        simple(searchKMP)
+    
+    def test_empty(self):
+        empty(searchKMP)
+
+    def test_equal(self):
+        equal(searchKMP)
+
+    def test_longer(self):
+        longer(searchKMP)
 
 class TestKR:
     def test_simple(self):
-        text = "AABBCCDDEEFFGGHHIIJJ"
-        word = "HH"
-        assert(searchKR(word, text)==14)
+        simple(searchKR)
+    
+    def test_empty(self):
+        empty(searchKR)
+
+    def test_equal(self):
+        equal(searchKR)
+
+    def test_longer(self):
+        longer(searchKR)
+        
