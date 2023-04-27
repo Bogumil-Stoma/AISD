1574a65b6020a02ac1b8a43df42bf8d0a5d5afd4
kjurczyn <01169180@pw.edu.pl>
2023-04-27 15:01:12 +0200

    added naive algorithm

--- pattern_sarch.py
+++ pattern_sarch.py
@@ -1,8 +1,17 @@
-def naive():
+
+
+def searchN(word: str, text: str) -> int:
+    w_len = len(word)
+    t_len = len(text)
+
+    for i in range(t_len - w_len + 1):
+        if text[i:i+w_len] == word:
+            return i
+    return -1
+
+
+def searchKMP():
     pass
 
-def KMP():
-    pass
-
-def KR():
+def searchKR():
     pass
